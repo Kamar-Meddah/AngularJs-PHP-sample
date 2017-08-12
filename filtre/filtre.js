fc0724bd169b6c4f44d48b6334a4142b2d4b4247
Kamar MEDDAH <Kamar-Meddah@users.noreply.github.com>
2017-08-12 18:09:57 +0100

    Delete filtre.js

--- filtre/filtre.js
+++
@@ -1,5 +0,0 @@
-    app.filter('contenu', function() {
-        return function(input) {
-            return input.contenu + " ...<br>" + '<a href="#!/category=' + input.categorie + '/post=' + input.titre + '/' + input.id + '">Voir la suite</a>';
-        }
-    })
